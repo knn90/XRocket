7385d0c436fcfc4a93f9e0f5bafceea73ed98280
knn <nguyenkhoi2190@gmail.com>
2021-04-16 10:44:44 +0800

    Init `XRocket` framework

---
+++ XRocket/XRocket/XRocket.h
@@ -0,0 +1,18 @@
+//
+//  XRocket.h
+//  XRocket
+//
+//  Created by Khoi Nguyen on 16/4/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XRocket.
+FOUNDATION_EXPORT double XRocketVersionNumber;
+
+//! Project version string for XRocket.
+FOUNDATION_EXPORT const unsigned char XRocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XRocket/PublicHeader.h>
+
+
