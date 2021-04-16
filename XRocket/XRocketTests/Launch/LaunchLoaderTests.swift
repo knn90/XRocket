063014bba9b0249399b4611ed7b4c93ea41580a4
knn <nguyenkhoi2190@gmail.com>
2021-04-16 20:02:41 +0800

    Deliver error when client error

--- XRocket/XRocketTests/Launch/LaunchLoaderTests.swift
+++ XRocket/XRocketTests/Launch/LaunchLoaderTests.swift
@@ -16,16 +16,30 @@ class LaunchLoader {
         self.request = request
     }
     
-    func load() {
-        client.load(from: request)
+    enum LoadError: Error {
+        case connectivity
+    }
+    
+    func load(completion: @escaping (LoadError?) -> Void) {
+        client.load(from: request) { error in
+            if error != nil {
+                completion(.connectivity)
+            }
+        }
     }
 }
 
 class HTTPClientSpy {
     var requestedURL: [URLRequest] = []
+    var completions = [(Error?) -> Void]()
     
-    func load(from request: URLRequest) {
+    func load(from request: URLRequest, completion: @escaping (Error?) -> Void) {
         requestedURL.append(request)
+        completions.append(completion)
+    }
+    
+    func completeWithError(_ error: Error, at index: Int = 0) {
+        completions[index](error)
     }
 }
 
@@ -36,24 +50,34 @@ class LaunchLoaderTests: XCTestCase {
         XCTAssertTrue(client.requestedURL.isEmpty)
     }
     
-    func test_load_sendRequestFromURL() {
+    func test_load_sendsRequestFromURL() {
         let request = URLRequest(url: URL(string: "http://a-specific-url.com")!)
         let (sut, client) = makeSUT(request: request)
         
-        sut.load()
+        sut.load { _ in }
         
         XCTAssertEqual(client.requestedURL, [request])
     }
     
-    func test_loadTwice_sendRequestFromURLTwice() {
+    func test_loadTwice_sendsRequestFromURLTwice() {
         let request = URLRequest(url: URL(string: "http://a-specific-url.com")!)
-        
         let (sut, client) = makeSUT(request: request)
         
-        sut.load()
-        sut.load()
+        sut.load { _ in }
+        sut.load { _ in }
         
         XCTAssertEqual(client.requestedURL, [request, request])
+    }
+    
+    func test_load_deliverErrorOnClientError() {
+        var error: LaunchLoader.LoadError?
+        let clientError = NSError(domain: "any NSError", code: 0)
+        let (sut, client) = makeSUT()
+        
+        sut.load { error = $0 }
+        client.completeWithError(clientError)
+        
+        XCTAssertEqual(error, .connectivity)
     }
     
     // MARK: - Helpers
