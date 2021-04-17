47b736ce9ace76e27205079c992dbc8db1bff269
knn <nguyenkhoi2190@gmail.com>
2021-04-17 18:00:08 +0800

    load request data from url

--- XRocket/XRocketTests/Shared/ImageDataLoaderTests.swift
+++ XRocket/XRocketTests/Shared/ImageDataLoaderTests.swift
@@ -9,6 +9,7 @@ import XCTest
 import XRocket
 
 class ImageDataLoader {
+    public typealias Result = Swift.Result<Data, Error>
     private let client: HTTPClient
     private let request: URLRequest
     
@@ -16,14 +17,38 @@ class ImageDataLoader {
         self.client = client
         self.request = request
     }
+    
+    public func load(completion: @escaping (Result) -> Void) {
+        client.load(from: request) { _ in
+            
+        }
+    }
 }
 
 class ImageDataLoaderTests: XCTestCase {
     func test_init_doesNotSendRequest() {
-        let request = anyURLRequest()
-        let client = HTTPClientSpy()
-        let _ = ImageDataLoader(client: client, request: request)
+        let (_, client) = makeSUT()
         
         XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let request = URLRequest(url: URL(string: "http://a-specific-url")!)
+        let (sut, client) = makeSUT(request: request)
+        
+        sut.load { _ in }
+        
+        XCTAssertEqual(client.requestedURLs, [request])
+    }
+    
+    // MARK: - Helpers
+    private func makeSUT(request: URLRequest = anyURLRequest(), file: StaticString = #file, line: UInt = #line) -> (ImageDataLoader, HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = ImageDataLoader(client: client, request: request)
+        
+        trackForMemoryLeak(client, file: file, line: line)
+        trackForMemoryLeak(sut, file: file, line: line)
+        
+        return (sut, client)
     }
 }
