3a724ed3ef0d77c99da0dc8ca9d12f44241caeaa
knn <nguyenkhoi2190@gmail.com>
2021-04-18 22:50:08 +0800

    Init `XRocketiOS` target

---
+++ XRocket/XRocketiOS/XRocketiOS.h
@@ -0,0 +1,18 @@
+//
+//  XRocketiOS.h
+//  XRocketiOS
+//
+//  Created by Khoi Nguyen on 18/4/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XRocketiOS.
+FOUNDATION_EXPORT double XRocketiOSVersionNumber;
+
+//! Project version string for XRocketiOS.
+FOUNDATION_EXPORT const unsigned char XRocketiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XRocketiOS/PublicHeader.h>
+
+
---
+++ XRocket/XRocketiOSTests/LaunchViewControllerTests.swift
@@ -0,0 +1,12 @@
+//
+//  LaunchViewControllerTests.swift
+//  XRocketiOSTests
+//
+//  Created by Khoi Nguyen on 18/4/21.
+//
+
+import XCTest
+
+class LaunchViewControllerTests: XCTestCase {
+    
+}
