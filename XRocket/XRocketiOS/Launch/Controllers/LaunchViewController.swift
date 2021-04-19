432b0cf5abc9fd4145c1d3715eb6e0fadb62d3de
knn <nguyenkhoi2190@gmail.com>
2021-04-19 12:37:02 +0800

    Launch request when user initiated a reload

---
+++ XRocket/XRocketiOS/Launch/Controllers/LaunchViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LaunchViewController.swift
+//  XRocketiOS
+//
+//  Created by Khoi Nguyen on 19/4/21.
+//
+
+import UIKit
+import XRocket
+
+public final class LaunchViewController: UITableViewController {
+    
+    public var loader: LaunchLoader?
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        title = LaunchPresenter.title
+        loadLaunches()
+    }
+    
+    @IBAction private func loadLaunches() {
+        loader?.load { _ in }
+    }
+}
--- XRocket/XRocketiOSTests/LaunchViewControllerTests.swift
+++ XRocket/XRocketiOSTests/LaunchViewControllerTests.swift
@@ -6,28 +6,9 @@
 //
 
 import XCTest
-import XRocket
 import UIKit
-
-class LaunchViewController: UIViewController {
-    
-    private let loader: LaunchLoader
-    
-    init(loader: LaunchLoader) {
-        self.loader = loader
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        title = LaunchPresenter.title
-        loader.load { _ in }
-    }
-}
+import XRocket
+import XRocketiOS
 
 class LaunchViewControllerTests: XCTestCase {
     func test_loadView_hasCorrectTitle() {
@@ -45,12 +26,20 @@ class LaunchViewControllerTests: XCTestCase {
         
         sut.loadViewIfNeeded()
         XCTAssertEqual(loader.loadLaunchCallCount, 1, "Expected first requests after view is loaded")
+        
+        sut.simulateUserInitiatedReload()
+        XCTAssertEqual(loader.loadLaunchCallCount, 2, "Expected another requests when user initiated reload")
+        
+        sut.simulateUserInitiatedReload()
+        XCTAssertEqual(loader.loadLaunchCallCount, 3, "Expected yet another requests when user initiated another reload")
     }
     
     // MARK: - Helpers
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (LaunchViewController, LoaderSpy) {
         let loader = LoaderSpy()
-        let sut = LaunchViewController(loader: loader)
+        let storyboard = UIStoryboard(name: "Launch", bundle: Bundle(for: LaunchViewController.self))
+        let sut = storyboard.instantiateInitialViewController() as! LaunchViewController
+        sut.loader = loader
         
         trackForMemoryLeak(sut, file: file, line: line)
         trackForMemoryLeak(loader, file: file, line: line)
@@ -74,5 +63,27 @@ class LaunchViewControllerTests: XCTestCase {
             XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
         }
         return value
+    }
+}
+
+extension UIRefreshControl {
+    func simulatePullToRefresh() {
+        simulate(event: .valueChanged)
+    }
+}
+
+extension UIControl {
+    func simulate(event: UIControl.Event) {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: event)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
+}
+
+extension LaunchViewController {
+    func simulateUserInitiatedReload() {
+        refreshControl?.simulatePullToRefresh()
     }
 }
