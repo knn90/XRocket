3d41caba8ad1ca7886243139a3f84990031ed74a
knn <nguyenkhoi2190@gmail.com>
2021-04-22 16:04:48 +0800

    Remove duplicate LaunchDateFormatter

--- XRocket/XRocketiOS/Launch/Views/LaunchCell.swift
+++ XRocket/XRocketiOS/Launch/Views/LaunchCell.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import XRocket
 
 public final class LaunchCell: UITableViewCell {
     @IBOutlet private(set) public var flightNumberLabel: UILabel!
@@ -15,13 +14,4 @@ public final class LaunchCell: UITableViewCell {
     @IBOutlet private(set) public var statusLabel: UILabel!
     @IBOutlet private(set) public var imageContainer: UIView!
     @IBOutlet private(set) public var rocketImageView: UIImageView!
-}
-
-private class LaunchDateFormatter {
-    static let dateFormatter = DateFormatter()
-    
-    static func format(from date: Date) -> String {
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        return dateFormatter.string(from: date)
-    }
 }
