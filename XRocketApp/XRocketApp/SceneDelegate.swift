1a0c4fdf29824a101adbd9cf9457b450678ed3c9
knn <nguyenkhoi2190@gmail.com>
2021-04-25 00:36:58 +0800

    Navigate to launch detail on launch select

--- XRocketApp/XRocketApp/SceneDelegate.swift
+++ XRocketApp/XRocketApp/SceneDelegate.swift
@@ -10,13 +10,14 @@ import XRocket
 import XRocketiOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    private lazy var client = URLSessionHTTPClient(session: .shared)
+    private lazy var imageLoader = RemoteImageDataLoader(client: client)
     var window: UIWindow?
     private var navigationController: UINavigationController?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
-        let client = URLSessionHTTPClient(session: .shared)
+        
         let url = URL(string: "https://api.spacexdata.com/v4/launches/query")!
         var launchRequest = URLRequest(url: url)
         launchRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -35,7 +36,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         launchRequest.httpBody = try! JSONSerialization.data(withJSONObject: params, options: .prettyPrinted)
         
         let launchLoader = RemoteLaunchLoader(client: client, request: launchRequest)
-        let imageLoader = RemoteImageDataLoader(client: client)
+        
         navigationController =  UINavigationController(
             rootViewController: LaunchUIComposer.composeWith(loader: launchLoader, imageLoader: imageLoader, didSelectLaunch: didSelectLaunch))
         
@@ -43,7 +44,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.makeKeyAndVisible()
     }
     
-    func didSelectLaunch(_ presentableLaunch: PresentableLaunch) {
+    func didSelectLaunch(_ launch: Launch) {
+
+        let launchDetailsViewController = LaunchDetailsUIComposer.composeWith(imageLoader: imageLoader, urls: launch.links.flickr.original)
+        navigationController?.pushViewController(launchDetailsViewController, animated: true)
     }
 }
 
